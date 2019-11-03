c7459b1f976f46cb2d75d3e8035dc5ad0f5ebc79
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2019-11-03 08:44:46 +0100

    modularize game

--- components/menu/start.js
+++ components/menu/start.js
@@ -2,7 +2,7 @@ import MenuEntry from './button'
 
 export default () => (
     <div>
-        <MenuEntry link='./game/index' label='Spielen' />
+        <MenuEntry link='./game/round_one' label='Spielen' />
         <MenuEntry link='./hiscores' label='Hiscores' />
         <MenuEntry link='./settings' label='Einstellungen' />
     </div>
